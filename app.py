8f5b51969f61ab9e1e3d6de588a60ccb34f410a7
Alex Kuznetsov <alex.kuznetsov@argen.energy>
2024-09-20 12:09:44 +0300

    hello alexk

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ def index():
     return 'Hello World!'
 @app.route('/hello')
 def hello():
-    return 'Hello World!'
+    return 'Hello Alex!'
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)
