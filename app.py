866783be7b5233211e34a05edf50c727c0a19125
Alex Kuznetsov <alex.kuznetsov@argen.energy>
2024-09-20 12:02:06 +0300

    Another routing

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World!'
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)
